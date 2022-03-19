fb5bd091ec9b7c43a61bb072ce19647851f9bcb9
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-03-18 23:52:31 -0500

    feat: add transaction json

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 //load routes
-var movieRoute = require('./route/movie.route')
+const movieRoute = require('./route/movie.route')
+const transactionRoute = require('./route/transaction.route')
 
 /* routes */
 
 //base route
 app.use('/api/', movieRoute)
+app.use('/api/', transactionRoute)
 
 //module conversion
 module.exports = app
